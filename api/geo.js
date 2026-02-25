fab4b3cf935b3eef420899fb32327b5330a9a4fe
RifqyAja <156778697+rydevs29@users.noreply.github.com>
2026-02-26 06:42:37 +0700

    Update geo.js

--- api/geo.js
+++ api/geo.js
@@ -1,15 +1,16 @@
-// api/geo.js
 export default function handler(req, res) {
-    // Mengambil data dari Vercel Edge Headers
-    const city = req.headers['x-vercel-ip-city'] || 'Unknown City';
-    const country = req.headers['x-vercel-ip-country'] || 'Unknown Country';
+    const city = req.headers['x-vercel-ip-city'] || 'Unknown';
+    const country = req.headers['x-vercel-ip-country'] || 'Unknown';
     const ip = req.headers['x-forwarded-for'] || '127.0.0.1';
-    const vercelRegion = req.headers['x-vercel-id'] || 'Local Dev';
+    const vercelRegion = req.headers['x-vercel-id'] ? req.headers['x-vercel-id'].split(':')[0] : 'Local';
+
+    res.setHeader('Cache-Control', 'no-store, max-age=0');
+    res.setHeader('Access-Control-Allow-Origin', '*');
 
     res.status(200).json({
         ip: ip,
         city: decodeURIComponent(city),
         country: country,
-        vercelRegion: vercelRegion.split(':')[0] // Mengambil kode region, misal 'sin1'
+        vercelRegion: vercelRegion
     });
 }
