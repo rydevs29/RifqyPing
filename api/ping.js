0bce8ce6a7b3fdd72052a6c00237bdb37e851594
RifqyAja <156778697+rydevs29@users.noreply.github.com>
2026-02-26 13:17:55 +0700

    Update ping.js

--- api/ping.js
+++ api/ping.js
@@ -1,7 +1,13 @@
 export default function handler(req, res) {
-    res.setHeader('Cache-Control', 'no-store, no-cache, must-revalidate, proxy-revalidate');
-    res.setHeader('Pragma', 'no-cache');
-    res.setHeader('Expires', '0');
+    res.setHeader('Cache-Control', 'no-store, max-age=0');
     res.setHeader('Access-Control-Allow-Origin', '*');
-    res.status(200).json({ status: 'ok', time: Date.now() });
+    
+    // Menghitung besar byte untuk fitur MTU tester
+    const payloadSize = req.body ? Buffer.byteLength(req.body, 'utf8') : 0;
+
+    res.status(200).json({ 
+        status: 'ok', 
+        time: Date.now(),
+        echo_size: payloadSize
+    });
 }
