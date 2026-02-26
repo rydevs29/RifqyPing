e9d3a31f7dfebc8deb2934efd0bb061833b1b5a1
RifqyAja <156778697+rydevs29@users.noreply.github.com>
2026-02-26 09:59:20 +0700

    Update geo.js

--- api/geo.js
+++ api/geo.js
@@ -1,16 +1,10 @@
 export default function handler(req, res) {
-    const city = req.headers['x-vercel-ip-city'] || 'Unknown';
-    const country = req.headers['x-vercel-ip-country'] || 'Unknown';
-    const ip = req.headers['x-forwarded-for'] || '127.0.0.1';
-    const vercelRegion = req.headers['x-vercel-id'] ? req.headers['x-vercel-id'].split(':')[0] : 'Local';
-
     res.setHeader('Cache-Control', 'no-store, max-age=0');
     res.setHeader('Access-Control-Allow-Origin', '*');
-
     res.status(200).json({
-        ip: ip,
-        city: decodeURIComponent(city),
-        country: country,
-        vercelRegion: vercelRegion
+        ip: req.headers['x-forwarded-for'] || 'Unknown',
+        city: decodeURIComponent(req.headers['x-vercel-ip-city'] || 'Unknown ISP/City'),
+        country: req.headers['x-vercel-ip-country'] || 'Unknown',
+        node: req.headers['x-vercel-id'] || 'Local/Unknown'
     });
 }
